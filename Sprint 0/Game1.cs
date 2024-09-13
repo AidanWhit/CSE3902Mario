3df4da0fa3f442a529adeb5d2acce3836c5b8f49
AidanWhit <aidanwhit@gmail.com>
2024-09-13 13:19:54 -0400

    Adding Mario State Patter Functionality.

--- Sprint 0/Game1.cs
+++ Sprint 0/Game1.cs
@@ -3,8 +3,11 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Sprint_0.Commands;
 using Sprint_0.Controls;
+using Sprint_0.Factories;
 using Sprint_0.Sprites;
+using Sprint_0.Sprites.MarioStates.LeftFacing.Mario;
 using System.Collections;
+using System.Diagnostics;
 
 namespace Sprint_0
 {
@@ -15,8 +18,9 @@ namespace Sprint_0
         private SpriteBatch spriteBatch;
         private ISprite currentSprite;
         private ISprite fontSprite;
-        private KeyboardControl keyControl;
-        private MouseControl mouseControl;
+
+
+        private Player mario;
 
         public Game1()
         {
@@ -27,25 +31,16 @@ namespace Sprint_0
 
         protected override void Initialize()
         {
-            keyControl = new KeyboardControl();
-            // Mouse Control still under construction
-            mouseControl = new MouseControl(this);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            Texture2D texture = Content.Load<Texture2D>("kirby 2");
-            fontSprite = new TextSprite(Content.Load<SpriteFont>("Credit"));
-            currentSprite = new Sprite1(texture, 1, 2);
-            mouseControl.getTexture(texture);
 
-            keyControl.RegisterCommand(Keys.D0, new QuitCommand(this));
-            keyControl.RegisterCommand(Keys.D1, new Sprite1Command(this, texture));
-            keyControl.RegisterCommand(Keys.D2, new Sprite2Command(this, texture));
-            keyControl.RegisterCommand(Keys.D3, new Sprite3Command(this, texture));
-            keyControl.RegisterCommand(Keys.D4, new Sprite4Command(this, texture));
+            MarioSpriteFactory.Instance.LoadAllContent(Content);
+
+            mario = new Player(new Vector2(400, 200));
         }
         protected override void UnloadContent()
         {
@@ -54,18 +49,16 @@ namespace Sprint_0
 
         protected override void Update(GameTime gameTime)
         {
-            keyControl.Update();
-            mouseControl.Update();
-            currentSprite.Update();
-
+            mario.Update(gameTime);
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            fontSprite.Draw(spriteBatch, new Vector2(100, 400));
-            currentSprite.Draw(spriteBatch, new Vector2(375, 200));
+            spriteBatch.Begin();
+            mario.Draw(spriteBatch, new Vector2(mario.XPos, mario.YPos));
+            spriteBatch.End();
             base.Draw(gameTime);
         }
 
