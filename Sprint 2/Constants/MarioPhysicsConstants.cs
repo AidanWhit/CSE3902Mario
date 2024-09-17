fdacff10923ce6127db669dbe364f624f44a60b3
AidanWhit <aidanwhit@gmail.com>
2024-09-16 20:20:52 -0400

    Adding File that contains constants used for Mario's Physics

---
+++ Sprint 2/Constants/MarioPhysicsConstants.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint_2.Constants
+{
+    public class MarioPhysicsConstants
+    {
+        public static Vector2 marioXVelocity = new Vector2(8f, 0);
+        //Jump Velocity is negative because y=0 is at the top of the screen
+        public static Vector2 marioJumpVelocity = new Vector2(0, -15f);
+        public static Vector2 marioFallVelocity = new Vector2(0, 1f);
+
+        public static float velocityDecay = 0.985f;
+        public static float maxXVelocity = 450f;
+        public static float maxJumpVelocity = -600f;
+        public static float maxFallVelocity = 500f;
+
+        public static int maxJumpHeight = 200;
+
+
+
+    }
+}
