546988498cd060c47a25fd51917a726e595e1ea1
Christian Blue <cblue918@gmail.com>
2024-10-02 20:21:02 -0400

    Update CollisionDetection.cs

--- Sprint 2/Collision/CollisionDetection.cs
+++ Sprint 2/Collision/CollisionDetection.cs
@@ -8,6 +8,12 @@ namespace Sprint_2.Collision
 {
     public class CollisionDetection
     {
+        /* TODO: things this needs to do:
+            1. sort static collideables into columns
+            2. reject obvious non-collisions using rectangle.intersects (returns a boolean)
+            3a. check list of movers against all collideables in adjacent columns
+            3b. check direction of collision using rectangle.intersect (returns a rectangle)  
 
+        */
     }
 }
