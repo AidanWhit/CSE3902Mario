428c1e1db320323a80cb486093cdd3ef3c4f12ad
AidanWhit <aidanwhit@gmail.com>
2024-11-06 13:21:09 -0500

    Forgot to add a file

---
+++ Sprint 2/Commands/CollisionCommands/ItemCollisionCommands/StaticCoinCollectCommand.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Sprint_2.GameObjects.ItemSprites;
+using Sprint_2.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint_2.Commands.CollisionCommands.ItemCollisionCommands
+{
+    public class StaticCoinCollectCommand : ICommands
+    {
+        private IPlayer player;
+        private StaticCoin coin;
+        private Rectangle collisionRect;
+
+        public StaticCoinCollectCommand(IPlayer player, StaticCoin coin, Rectangle collisionRect)
+        {
+            this.player = player;
+            this.coin = coin;
+            this.collisionRect = collisionRect;
+        }
+
+        public void Execute()
+        {
+            HUD.Instance.AddScoreFromCoin(200);
+        }
+    }
+}
